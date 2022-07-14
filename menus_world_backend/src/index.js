8ad535f8bddad6ddcc0a28a95edb01cb0b76b82e
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-07-14 15:33:14 -0500

    add CORS

--- menus_world_backend/src/index.js
+++ menus_world_backend/src/index.js
@@ -2,11 +2,13 @@ require("dotenv").config(); // las variables del .env se agregan a process.env
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routerMenus = require("./routers/menu.router.js");
 const userRouter = require("./routers/user.router.js");
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 server.use("/menu", routerMenus);
 server.use("/users", userRouter);
@@ -18,7 +20,6 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 
-//const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 function crearMiddlware() {
