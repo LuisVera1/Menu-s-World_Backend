bdfea267bbfe6193748168fd6fa2bb37116e3132
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-07-21 16:53:49 -0500

    add function admins

--- menus_world_backend/src/index.js
+++ menus_world_backend/src/index.js
@@ -6,12 +6,14 @@ const cors = require("cors");
 
 const routerMenus = require("./routers/menu.router.js");
 const userRouter = require("./routers/user.router.js");
+const adminRouter = require("./routers/adminUser.router.js");
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 server.use("/menu", routerMenus);
 server.use("/users", userRouter);
+server.use("/admin", adminRouter);
 
 const PORT = process.env.PORT;
 
