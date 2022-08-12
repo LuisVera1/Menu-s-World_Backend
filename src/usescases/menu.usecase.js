23b1a88a0f638136d082a642a4dffe1ff73b7fee
Nestor Ramirez <nesjesramrey2@gmail.com>
2022-08-12 00:01:07 -0500

    fix mistake

--- src/usescases/menu.usecase.js
+++ src/usescases/menu.usecase.js
@@ -17,7 +17,7 @@ async function createMenu(menu) {
 async function updateMenu(id, menu) {
   const filter = {
     id: id,
-
+  };
   await Menu.findOneAndUpdate(filter, menu);
 
   const updatedMenu = Menu.findOne(filter);
